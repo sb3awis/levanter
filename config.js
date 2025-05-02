06b2e34cc0a6faa9747f7d4741928cd9235220ce
sb3awis <fardmax78@gmail.com>
2025-05-02 01:27:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   PREFIX: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || '212656317785',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
